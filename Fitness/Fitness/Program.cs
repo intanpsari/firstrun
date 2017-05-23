f88ac1ece5668dfa5bed30adbbdbc40a59c7e8f9
Intan Permatasari <Intan Permatasari@DESKTOP-N1FKJCQ>
2017-05-23 11:40:48 +0700

    update1

---
+++ Fitness/Fitness/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String input="";
+            int jumlah = 0;
+
+            //looping
+            //keluar bila user masukkin "quit"
+            while (true)
+            {
+                //keluarkan pertanyaanya
+                Console.WriteLine("Berapa lama lari?");
+
+                //minta input dari user
+                input = Console.ReadLine();
+                
+                //menambahkan inputnya
+                jumlah = jumlah + Int32.Parse(input);
+
+                if (input == "quit")
+                {
+                    break;
+                }
+                
+                //ngerlurin inputnya
+                Console.WriteLine("Selamat anda sudah lari " + jumlah + "menit");
+
+            }
+            
+
+
+
+        }
+    }
+}
